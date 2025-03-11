2aa559be1b66cabc61ad61f038d33c84f9b59932
malloc <malloc_3141592@naver.com>
2025-03-11 10:07:45 +0900

    complete

--- Math.fs
+++ Math.fs
@@ -6,4 +6,11 @@ open System
 /// 32-bit integer, and returns the number of steps it takes to reach 1 in the
 /// Collatz sequence.
 let collatz n =
-  0
+    let rec iter n step =
+
+        if n = 1UL then step
+        elif n % 2UL = 0UL then iter (n / 2UL) (step + 1)
+        else iter (3UL * n + 1UL) (step + 1)
+
+    let N = uint64 n
+    iter N 0
